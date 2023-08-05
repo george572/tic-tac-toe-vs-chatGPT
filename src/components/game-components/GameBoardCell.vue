871b24101d7994467a8664abb6bc9110628fc742
Giorgikhutiashvili <g.khutiashvili@gmail.com>
2023-08-06 03:24:24 +0400

    impltemented board logic, player turns and countdown to determine who starts

--- src/components/game-components/GameBoardCell.vue
+++ src/components/game-components/GameBoardCell.vue
@@ -1,13 +1,54 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+import { useGameStateStore } from '@/stores/gameState';
+
+const store = useGameStateStore();
+const activateUserSymbol = ref<boolean>(false);
+const activateGptSymbol = ref<boolean>(false);
+const props = defineProps({
+  occupied : { type: Boolean, required: true },
+  player: { type: String, required: false, default: '' },
+  cellId: { type: String, required: true }
+});
+
+const emit = defineEmits(['cell-picked']);
+
+const pickCell = () => {
+  if (!props.occupied) {
+    emitPickCellEvent();
+    store.updateGameState(props.player, props.cellId);
+  }
+
+};
+
+const emitPickCellEvent = () => {
+  emit('cell-picked');
+  if ( props.player === 'user') {
+    activateUserSymbol.value = true;
+  } else {
+    activateGptSymbol.value = true;
+  }
+};
 
 </script>
 
 <template>
-  <div class="cell">
+  <div
+    class="cell inactive"
+    :class="{'active' : occupied}"
+    @click="pickCell()"
+  >
     <img
-      src="../../../public/favicon.ico"
+      v-if="activateUserSymbol"
+      src="/src/assets/images/circle.svg"
       alt=""
     >
+    <img
+      v-else-if="activateGptSymbol"
+      src="/src/assets/images/x.svg"
+      alt=""
+    >
+    <div v-else />
   </div>
 </template>
 
@@ -19,5 +60,23 @@
   display: flex;
   align-items: center;
   justify-content: center;
+  transition: all 300ms;
+  img {
+    width: 80px;
+  }
+}
+.inactive {
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.3);
+    cursor: pointer;
+  }
+}
+
+.active {
+  background-color: rgba(0, 0, 0, 0.5);
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.5);
+    cursor: default;
+  }
 }
 </style>
